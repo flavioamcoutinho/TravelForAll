b18fd55533685ca6a49f60c72198b648b970a1ed
flavioamcoutinho <33954555+flavioamcoutinho@users.noreply.github.com>
2018-05-22 09:22:29 +0100

    Change drawer

--- app/src/main/java/meuprimeiroapp/studio/flavi/travelforall/MainActivity.java
+++ app/src/main/java/meuprimeiroapp/studio/flavi/travelforall/MainActivity.java
@@ -80,17 +80,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_preferencias) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_viajar) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_historico) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_login) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_registar) {
 
         }
 
